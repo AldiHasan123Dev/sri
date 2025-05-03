cc6b2e2a24ab68bf44843683a3dfee8485fd806a
Arissury <email@anda.com>
2025-05-03 15:01:42 +0700

    update navbar

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
+    <link href="{{ asset('build/assets/img/logo2.jpeg') }}" rel="icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
--- resources/views/components/landingpage/footer.blade.php
+++ resources/views/components/landingpage/footer.blade.php
@@ -70,6 +70,8 @@
     </div>
 
   </footer>
+  {{-- Icon wa --}}
+  {{-- <a href="" id="icon-wa" --}}
 
   <!-- Scroll Top -->
   <a href="#" id="scroll-top" class="scroll-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
